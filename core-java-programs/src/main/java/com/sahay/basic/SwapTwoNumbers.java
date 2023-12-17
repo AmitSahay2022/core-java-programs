1d9ff533f1fe02996fd2eb68b5bf13f739bf1a59
Sahay Software <sahay.mtech2013@gmail.com>
2023-12-17 14:01:14 +0530

    Maven Project created

---
+++ core-java-programs/src/main/java/com/sahay/basic/SwapTwoNumbers.java
@@ -0,0 +1,18 @@
+package com.sahay.basic;
+/*
+ * Swap two numbers
+ */
+public class SwapTwoNumbers {
+	
+	public static void swap(int a,int b) {
+		System.out.println("Before swapping: "+a+" "+b);
+		int temp=a; //now a is safe
+		a=b; //now value of b is safe inside a
+		b=temp; 
+		System.out.println("After swapping: "+a+" "+b);
+	}
+	
+	public static void main(String[] args) {
+        swap(5,8);
+	}
+}
